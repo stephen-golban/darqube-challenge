59c4b76b96f4e3b1d06d3f0125aac2a0ed52935c
sgolban <sgolban@qoobus.com>
2021-11-03 17:07:28 +0200

    fixing the prod err

--- api/base.ts
+++ api/base.ts
@@ -1,7 +1,5 @@
 import axios from 'axios'
 
-const { NEXT_PUBLIC_API_BASE_URL } = process.env
-
 export const API = axios.create({
-  baseURL: NEXT_PUBLIC_API_BASE_URL,
+  baseURL: 'https://finnhub.io/api/v1',
 })
